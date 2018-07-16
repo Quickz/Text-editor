a30b71c67c6e277483f32378c270f33768a4ce59
Quickz <alexserg@inbox.lv>
2018-07-16 21:39:41 +0300

    added an ability to save the text content to a file

--- src/main/MainPage.java
+++ src/main/MainPage.java
@@ -2,9 +2,20 @@ package main;
 
 import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class MainPage
 {
+    @FXML
+    public TextArea content;
+
     public void newFile()
     {
         System.out.println("new");
@@ -17,12 +28,76 @@ public class MainPage
 
     public void save()
     {
-        System.out.println("save");
+        // TODO:
+        // add ability to save a file without
+        // repeatedly having to open a file dialog
+        // to specify the path
+        saveAs();
     }
 
     public void saveAs()
     {
-        System.out.println("save as");
+        Stage fileDialog = new Stage();
+        FileChooser fileChooser= new FileChooser();
+        fileChooser.setTitle("Save As");
+        fileChooser.getExtensionFilters().add(
+            new FileChooser.ExtensionFilter(
+                "Text Documents (*.txt)", ".txt"));
+
+        File file = fileChooser.showSaveDialog(fileDialog);
+
+        if (file != null)
+        {
+            saveContent(file.getPath());
+        }
+    }
+
+    /**
+     * saves the text inside
+     * the text editor to the specified path
+     **/
+    private void saveContent(String path)
+    {
+        FileWriter writer = null;
+        BufferedWriter bufferedWriter = null;
+
+        try
+        {
+            writer = new FileWriter(path);
+            bufferedWriter = new BufferedWriter(writer);
+
+            String contentText = content
+                .getText()
+                .replaceAll("\n", System.lineSeparator());
+
+            bufferedWriter.write(contentText);
+        }
+        catch (IOException e)
+        {
+            System.out.println(
+                "Error in writing to a file: " +
+                e.getMessage());
+        }
+        finally
+        {
+            try
+            {
+                if (bufferedWriter != null)
+                {
+                    bufferedWriter.close();
+                }
+                if (writer != null)
+                {
+                    writer.close();
+                }
+            }
+            catch (IOException e)
+            {
+                System.out.println(
+                    "Error in closing writer: " +
+                    e.getMessage());
+            }
+        }
     }
 
     public void exit()
